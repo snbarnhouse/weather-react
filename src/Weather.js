52acfa95b5eaf285da21ad37b0205a46e878abfe
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-10 09:50:40 -0500

    Adding in weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-    function handleResponse(response) {
-         alert (`The Weather in ${response.data.name} is ${Math.round(response.data.main.temp)}°F.`);
-    }
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
     let apiKey = "1743d71cea491649f0bd96f06af46d71";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from Weather</h2>
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°F</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind} mph</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
+
+// import React from "react";
+// import axios from "axios";
+
+// export default function Weather(props) {
+//     function handleResponse(response) {
+//          alert (`The Weather in ${response.data.name} is ${Math.round(response.data.main.temp)}°F.`);
+//     }
+//     let apiKey = "1743d71cea491649f0bd96f06af46d71";
+//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+//     axios.get(apiUrl).then(handleResponse);
+//     return <h2>Hello from Weather</h2>
+// }
